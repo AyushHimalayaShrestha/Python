25632b0c5ef4d680029684fe7f089f49abcd9ce7
AyushHimalayaShrestha <aayush625@gmail.com>
2025-02-12 21:39:39 +0545

    ifelse

---
+++ ifelse.py
@@ -0,0 +1,31 @@
+# is_hot=False
+# is_cold=False
+# if is_hot:
+#     print("It's a hot day")
+#     print("Drink plenty of water")
+# elif is_cold:
+#     print("It's a cold day")
+#     print("Wear warm clothes.")
+    
+# else:
+#     print("It's a lovely day.")
+   
+# print("Enjoy your day")
+
+
+# price=1000000
+# has_good_credit=True
+
+# if has_good_credit:
+#     down_payment=0.1*price
+
+# else:
+#     down_payment=0.2*price
+# print(f"Down Payment: Rs{down_payment}")
+
+has_high_income =True
+has_good_credit=True
+
+if has_high_income and has_good_credit:
+    print("Eligible for Loan")
+
