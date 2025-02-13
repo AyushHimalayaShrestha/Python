1dcd394612b91cc795db4c7592369cedb71c8131
AyushHimalayaShrestha <aayush625@gmail.com>
2025-02-13 20:53:21 +0545

    function

---
+++ function.py
@@ -0,0 +1,28 @@
+#function -> is a block of code to perform the particular task
+#define the function
+def demoFunction():
+    print('Hello World')
+
+
+
+#call the function
+demoFunction()
+
+#function with arguements
+
+#define the function with parameter
+def add(x,y):
+    sum=x+y
+    print(sum)
+#call the function
+add(20,30)
+
+#function with return type
+#define the function
+def testFunction(num1,num2):
+    return num1*num2
+
+
+#call the function
+result=testFunction(15,10)
+print(result)
