a4469fa8cebede788558153591a890aafac1b50b
AyushHimalayaShrestha <aayush625@gmail.com>
2025-02-14 06:57:15 +0545

    strings indexing

---
+++ strings.py
@@ -0,0 +1,36 @@
+text='Welcome Home baby'
+print (text)
+
+#indexing
+print(text[3])
+
+#negative indexing
+print(text[-3])
+#slice
+print(text[0:12])
+print(text[5:])
+print(text[:12])
+print(text[:])
+
+#negative slice
+print(text[-10:-2])
+
+#count the number of length in string
+print(len(text))
+
+#upper()
+print(text.upper())
+
+#lower()
+print(text.lower())
+
+#split()
+print(text.split())
+print(text.split('baby'))
+
+
+
+
+
+
+
