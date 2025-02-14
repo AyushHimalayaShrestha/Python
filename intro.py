fdf782132e92cf5aab305019fe7ae8498ac992ae
AyushHimalayaShrestha <aayush625@gmail.com>
2025-02-14 06:51:07 +0545

    data types

---
+++ intro.py
@@ -0,0 +1,32 @@
+print('Hello World !')
+# variable  -> container to store value
+fullName='Ayush Himalaya Shrestha'
+print('Hello',fullName)
+#to check data type use type()
+print(type(fullName))
+# data type in python
+#integer(int)
+#float
+#boolean
+#string(str)
+#list -> collection of data (mutable ie element inside list can be changed)
+#tuple-> imutable 
+#set
+#dictionary ->{'key':'value'}
+
+fruits=['apple', 'banana','cherry','orange']
+print(fruits)
+
+#to convert list into tuple
+t=tuple(fruits)
+print(t)
+print(type(t))
+
+#arthematic operator
+#+,-,*,%,/,//
+
+#comparison operator
+# !=, ==
+
+#assignment operator
+# += a+=b is same as a=a+b,-=,*=,
