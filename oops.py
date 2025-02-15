6db81e6641c3aa9848a6d57ea9ab7fdb1942effd
AyushHimalayaShrestha <aayush625@gmail.com>
2025-02-15 21:27:11 +0545

    oops

---
+++ oops.py
@@ -0,0 +1,12 @@
+#class -> it is blueprint that defines the nature of the future object
+print(type(12))
+print(type('Hello World'))
+
+class Student:
+    pass
+
+#object -> variable created from the class
+Ayush=Student()
+print(type(Ayush))
+
+
